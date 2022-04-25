c331889282590436b045cd5b78ab2fd9bafe5625
jediProgramer <erlangga.kmoekasan@gmail.com>
2022-04-25 16:28:22 +0700

    first commit

---
+++ src/main/java/com/erlanggstudio/sisteminformasitokohp/Main.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.erlanggstudio.sisteminformasitokohp;
+
+import com.erlanggstudio.sisteminformasitokohp.view.ViewCRUD;
+
+/**
+ *
+ * @author erlanga
+ */
+public class Main  {
+    public static void main(String[] args) {
+        ViewCRUD viewCRUD = new ViewCRUD();
+        viewCRUD.showMenu(); 
+    }
+}
